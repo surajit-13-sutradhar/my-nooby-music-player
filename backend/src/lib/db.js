3ab9777cfe3a139351c379be3e9dbc29377f9b09
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2025-02-16 19:39:04 +0530

    First Commit

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+	try {
+        // Connect to MongoDB
+		const conn = await mongoose.connect(process.env.MONGODB_URI)
+        // Log the connection host
+		console.log(`Connected to MongoDB ${conn.connection.host}`);
+	} catch (error) {
+		console.log("Failed to connect to MongoDB", error);
+		process.exit(1); // 1 is failure, 0 is success
+	}
+};
